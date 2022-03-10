48b2a335992cc9cd10b2671db9cc3576fd143d71
PalMacRobotics <palmacrobotics@gmail.com>
2022-03-10 08:28:12 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "zahS4l3fLCLHKQPlHn5JNFZc0CDcfhYmHUy7lNkD4ThywptuYYeBjZ5kzNeM7HXA";
 
 /**
  * Get list of teams in event

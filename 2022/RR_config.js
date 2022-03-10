de6a4ed94d75228896570c05a37bbd1b6044f69b
PalMacRobotics <palmacrobotics@gmail.com>
2022-03-10 09:10:48 -0500

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022flwp",
+        "defaultValue":"2022nyro",
         "required":"true",
         "disabled":"true"
       },
